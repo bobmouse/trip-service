26abae41115866569ee11b23fdab698303130c6a
Alexander Gordeev <bobmouse.g@gmail.com>
2012-02-02 12:45:35 +0400

    Base project files

---
+++ src/main/java/go/tripservice/server/model/entity/TestEntity.java
@@ -0,0 +1,11 @@
+package go.tripservice.server.model.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: gordeev
+ * Date: 01.02.12
+ * Time: 18:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestEntity {
+}
---
+++ src/main/java/go/tripservice/server/web/controller/TestController.java
@@ -0,0 +1,11 @@
+package go.tripservice.server.web.controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: gordeev
+ * Date: 01.02.12
+ * Time: 17:04
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestController {
+}
