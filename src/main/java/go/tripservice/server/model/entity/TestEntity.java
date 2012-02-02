3b1060fca4946c5308e03f28cd7741c762555a89
Alexander Gordeev <bobmouse.g@gmail.com>
2012-02-02 12:55:36 +0400

    Test entity

--- src/main/java/go/tripservice/server/model/entity/TestEntity.java
+++ src/main/java/go/tripservice/server/model/entity/TestEntity.java
@@ -1,10 +1,28 @@
 package go.tripservice.server.model.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * User: bobmouse
  * Date: 01.02.12
  * Time: 18:08
  * This is the test entity
  */
+@Entity
+@Table(name = "TS_TESTS")
 public class TestEntity {
+    private Long id;
+
+    @Id
+    @Column(name = "ID")
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
